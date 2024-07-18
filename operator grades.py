d77a443ef717ea4f3a211742014a21488832d7c3
tamanna-1123 <sonitamanna155@gmail.com>
2024-07-18 06:54:02 -0400

    Add files via upload

---
+++ operator grades.py
@@ -0,0 +1,13 @@
+x=int(input("enter marks for grade"))
+if x>=80:
+    print("A")
+elif 70<=x<=79:
+    print("B")
+elif 60<=x<=69:
+    print("C")
+elif 50<=x<=59:
+    print("D")            
+elif 40<=x<=49:
+    print("E")
+else:
+    print("Fail")        
---
+++ operator marks.py
@@ -0,0 +1,9 @@
+x=int(input("enter the marks"))
+if x>=70:
+    print("distinction")
+elif 60<x<=69:
+    print("merit")
+elif 40<x<=59:
+    print("pass")
+else:
+    print("fail")
